213146025c55176e1ab34b6cecf1abcfa5e5f026
copapp <93282742+copapp@users.noreply.github.com>
2021-10-27 11:15:48 -0700

    change

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 echo "[General]
 path_to_gclone = ./gclone
-telegram_token = $telegram_token
-user_ids = $user_ids
-group_ids = $group_ids
-gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
+telegram_token = 2022284744:AAGKipd-YYt-9vyRaAmcwcd_mH6HDOe0fdY
+user_ids = 1303785554,2057591155
+group_ids = -625060355
+gclone_para_override = 
 npm install http-server -g
 http-server -p $PORT &
 python3 telegram_gcloner/telegram_gcloner.py
